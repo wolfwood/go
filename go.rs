2d232db252a82568179580c07ac630c4663f0f4d
wolfwood <wolfwood@xomb.org>
2013-01-02 22:47:20 -0500

    now, with a board type and dynamic sizing

--- go.rs
+++ go.rs
@@ -1,12 +1,18 @@
+// --- imports ---
 use vec::*;
 use to_str::ToStr;
 
+// --- types ---
 enum Liberty{
     Black,
     White,
     Empty
 }
 
+enum Board = ~[~[Liberty]];
+
+
+// --- implementations ---
 impl Liberty: ToStr{
     pure fn to_str() -> ~str{
         match self{
@@ -17,8 +23,46 @@ impl Liberty: ToStr{
     }
 }
 
+impl Board{
+    static fn new(num_rows: uint) -> Board{
+        let mut first_row = ~[Empty];
+        first_row.grow(num_rows -1, &Empty);
 
+        let mut rows = ~[copy first_row];
+        rows.grow(num_rows -1, &first_row);
 
+        Board(rows)
+    }
+
+    fn print(&self){
+        let mut i = 0, j = 0;
+        for each(**self) |l| {
+            io::print(fmt!(" %d ", i));
+            j = 0;
+
+            for each(*l) |k| {
+                io::print(k.to_str());
+                j += 1;
+            }
+
+            io::println("");
+
+            i += 1;
+        }
+
+        io::println("");
+        io::print("   ");
+        i = 0;
+        while i < j {
+            io::print(fmt!(" %d", i));
+            i+=1;
+        }
+
+        io::println("");
+    }
+}
+
+// --- the beef ---
 fn main(){
     //let rows = 9;
     let mut board = [[ Empty, ..9 ], ..9 ];
@@ -29,12 +73,6 @@ fn main(){
         }
     }
 
-
-    for each(board) |l| {
-        for each(*l) |k| {
-            io::print(k.to_str());
-        }
-
-        io::println("");
-    }
+    let b = Board::new(9);
+    b.print();
 }
